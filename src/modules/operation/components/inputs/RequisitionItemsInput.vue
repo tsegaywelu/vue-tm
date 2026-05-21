ab4063b1b94076f09045f62e48d436fe0052a3e1
Natty27 <natnaelalem000@gmail.com>
2026-05-21 07:15:13 +0300

    fix: update requisition items input search_key to use regex matching

--- src/modules/operation/components/inputs/RequisitionItemsInput.vue
+++ src/modules/operation/components/inputs/RequisitionItemsInput.vue
@@ -123,7 +123,7 @@ function remove(id: string) {
                   url="/inventory-items"
                   label_key="name"
                   value_key="_id"
-                  search_key="name"
+                  search_key="name[regexAny]"
                   searchable
                   :display_value="item.itemName"
                   @select="(opt: any) => (item.itemName = opt.label)"
